a355dcdb3fe9ec05641f1179b7cd8070cda81770
gedvydas <74902029+Ged-J@users.noreply.github.com>
2022-11-29 12:37:01 +0000

    Topic 10 - Exercises

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -19,9 +19,13 @@ router.post('/',asyncHandler( async (req, res, next) => {
       return next();
     }
     if (req.query.action === 'register') {
-      await User.create(req.body);
-      res.status(201).json({code: 201, msg: 'Successful created new user.'});
-    } else {
+        if(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/.test(req.body.password)) {
+            await User.create(req.body).catch(next);
+        } else {
+            res.status(401).json({code: 401,msg: 'Weak password.'});
+        }
+        res.status(201).json({code: 201, msg: 'Successfully created new user.'});
+      } else {
       const user = await User.findByUserName(req.body.username);
         if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
         user.comparePassword(req.body.password, (err, isMatch) => {
@@ -56,7 +60,9 @@ router.post('/:userName/favourites', asyncHandler(async (req, res) => {
     const userName = req.params.userName;
     const movie = await movieModel.findByMovieDBId(newFavourite);
     const user = await User.findByUserName(userName);
-    await user.favourites.push(movie._id);
+    if(!await user.favourites.includes(movie.id)){
+        await user.favourites.push(movie._id);
+    }
     await user.save(); 
     res.status(201).json(user); 
   }));
