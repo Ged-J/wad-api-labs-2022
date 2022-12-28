0e591d3729df6dd218006c1cdf80dc4cc9f1a9eb
gedvydas <74902029+Ged-J@users.noreply.github.com>
2022-12-28 15:00:52 +0000

    Topic 11 - end of labs

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes, Link } from "react-router-dom";
 import { PublicPage, Movies, Profile, HomePage } from "./pages";
+import LoginPage from "./loginPage";
+import AuthProvider from "./authContext";
+import PrivateRoute from "./privateRoute";
+import AuthHeader from "./authHeader";
+import SignUpPage from "./signUpPage";
+import MoviesProvider from "./moviesContext";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <AuthProvider>
+        <AuthHeader />
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -20,13 +28,32 @@ const App = () => {
             <Link to="/profile">Profile</Link>
           </li>
         </ul>
-        <Switch>
-          <Route path="/public" component={PublicPage} />
-          <Route exact path="/" component={HomePage} />
-          <Route path="/movies" component={Movies} />
-          <Route path="/profile" component={Profile} />
-          <Redirect from="*" to="/" />
-        </Switch>
+        <MoviesProvider>
+          <Routes>
+            <Route path="/public" element={<PublicPage />} />
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/movies"
+              element={
+                <PrivateRoute>
+                  <Movies />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </MoviesProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
